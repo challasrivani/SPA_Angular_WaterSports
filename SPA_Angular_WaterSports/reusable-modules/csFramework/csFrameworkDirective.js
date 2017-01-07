0c74405d8ec155e9d267303c40de528a7af3c9c4
Sri Challa <srivanichalla83@gmail.com>
2017-01-08 09:59:41 +1100

    addAttrtoDirective_and_customizeTitleandotherfields

--- SPA_Angular_WaterSports/reusable-modules/csFramework/csFrameworkDirective.js
+++ SPA_Angular_WaterSports/reusable-modules/csFramework/csFrameworkDirective.js
@@ -5,7 +5,9 @@ angular.module("csFramework").directive("csFramework", function () {
     return {
         transclude: false,
         scope: {
-
+            title: '@',
+            subtitle: '@',
+            iconSource: '@'
         },
         controller: "csFrameworkController",
         templateUrl: "reusable-modules/csFramework/csFrameworkTemplate.html"
