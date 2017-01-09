307c26e105ebd438137048201e06a7969edcc428
Sri Challa <srivanichalla83@gmail.com>
2017-01-09 16:03:10 +1100

    openingandclosgingmenu_multiplemenulevels

---
+++ SPA_Angular_WaterSports/reusable-modules/csMenu/csMenuGroupDirective.js
@@ -0,0 +1,30 @@
+﻿"use strict";
+
+angular.module("csMenu").directive("csMenuGroup", function () {
+    return {
+        require: '^csMenu',
+        transclude: true,
+        controller: "csMenuController",
+        templateUrl: "reusable-modules/csMenu/csMenuGroupTemplate.html",
+        scope: {
+            label: '@',
+            icon: '@'
+
+        },
+        link: function (scope, el, attr, ctrl) {
+
+            scope.isOpen = false;
+
+            scope.closeMenu = function() {
+                scope.isOpen = false;
+            }
+
+            scope.clicked = function () {
+                scope.isOpen = !scope.isOpen;
+            }
+
+        }
+
+    };
+
+});
